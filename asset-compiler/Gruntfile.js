41bb4a5875a5e3e949310538d5193b1c5d53cad2
Rabo <xiepeng1992@gmail.com>
2014-06-04 19:12:08 +0800

    magnum theme version 1,added animation of header and posts.Added music player of blog

---
+++ asset-compiler/Gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+            less: {
+                files: [
+                    'LESS/*',
+                    'bower_components/normalize-css/normalize.css'
+                ],
+                tasks: ['less']
+            },
+            css: {
+                files: [
+                    'compiled_css/*'
+                ],
+                tasks: ['cssmin']
+            }
+        },
+
+        uglify: {
+            options: {
+                mangle: false
+            },
+            combinejs: {
+                files: {
+                    '../assets/js/all.min.js': [
+                        'bower_components/modernizr/modernizr.js',
+                        'custom_components/responsive_iframes/responsive_iframes.js',
+                        'custom_components/magnum.js',
+                        'custom_components/jquery.appear.js'
+                    ]
+                }
+            }
+        },
+
+        less: {
+            components: {
+                files: {
+                    'compiled_css/compiled_css.css': ['LESS/less_imports.less']
+                }
+            },
+            options: {
+                expand: true,
+                paths: [
+                    'bower_components/lesshat',
+                    'LESS'
+                ]
+            }
+        },
+
+        cssmin: {
+            combine: {
+                files: {
+                    '../assets/css/style.css': ['bower_components/normalize-css/normalize.css', 'compiled_css/compiled_css.css']
+                }
+            }
+        },
+    });
+
+
+    grunt.registerTask('build', [
+        'less',
+        'cssmin',
+        'uglify'
+    ]);
+    // Load grunt plugins.
+    grunt.loadNpmTasks('assemble-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+};
